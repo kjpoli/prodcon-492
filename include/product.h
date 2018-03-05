7f16651e8021f3291cf4e79c4ed09221fd0fea0b
Philip Vitale <philipmvitale@gmail.com>
2018-03-05 12:52:08 -0500

    remove producer header

--- include/product.h
+++ include/product.h
@@ -1,18 +1,16 @@
-#ifndef PRODCON_PRODUCT_H_
-#define PRODCON_PRODUCT_H_
+#ifndef INCLUDE_PRODUCT_H_
+#define INCLUDE_PRODUCT_H_
 
 class Product {
-    private:
-        int id;
-        time_t time_stamp;
-        unsigned int life;
     public:
         Product();
         Product(int id);
-        
         int getProductId();
         time_t getProductTime();
         unsigned int getProductLife();
+    private:
+        int id;
+        time_t time_stamp;
 }
 
-#endif  // PRODCON_PRODUCT_H_
+#endif  // INCLUDE_PRODUCT_H_
